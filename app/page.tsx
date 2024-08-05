62a6d404f18f67baad4bee93bd3e68ffe448cad3
krutikmaru <krutikmaru18@gmail.com>
2024-08-05 11:41:59 +0530

    Globe

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,16 @@ import Details from "@/app/ui/components/Home/Details/Details";
 import Customers from "@/app/ui/components/Home/Customers/Customers";
 import Services from "@/app/ui/components/Home/Services/Services";
 import Benefits from "./ui/components/Home/Benefits/Benefits";
+import Globe from "@/components/magicui/globe";
 
 export default function Home() {
   return (
     <div className="">
+      {/* <div className="absolute w-full bottom-0 overflow-y-hidden"> */}
+      <div className="absolute flex h-full w-full  items-center justify-center rounded-lg  px-40 pb-40 pt-8 md:pb-60 ">
+        <Globe className="top-24  " />
+      </div>
+      {/* </div> */}
       <Landing />
       <Details />
       <Customers />
